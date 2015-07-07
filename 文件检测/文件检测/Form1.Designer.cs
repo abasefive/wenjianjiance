e54a8792a856160e1f81681de9f97ceb064a6274
abasefive <33124974@qq.com>
2015-07-07 23:02:38 +0800

    测试
    测试用的

---
+++ 文件检测/文件检测/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace 文件检测
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_mulu = new System.Windows.Forms.TextBox();
+            this.liulan = new System.Windows.Forms.Button();
+            this.textBox_mubiao = new System.Windows.Forms.TextBox();
+            this.mubiao = new System.Windows.Forms.Button();
+            this.begin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_mulu
+            // 
+            this.textBox_mulu.Location = new System.Drawing.Point(12, 25);
+            this.textBox_mulu.Name = "textBox_mulu";
+            this.textBox_mulu.Size = new System.Drawing.Size(353, 21);
+            this.textBox_mulu.TabIndex = 0;
+            // 
+            // liulan
+            // 
+            this.liulan.Location = new System.Drawing.Point(371, 25);
+            this.liulan.Name = "liulan";
+            this.liulan.Size = new System.Drawing.Size(75, 23);
+            this.liulan.TabIndex = 1;
+            this.liulan.Text = "浏览";
+            this.liulan.UseVisualStyleBackColor = true;
+            this.liulan.Click += new System.EventHandler(this.liulan_Click);
+            // 
+            // textBox_mubiao
+            // 
+            this.textBox_mubiao.Location = new System.Drawing.Point(12, 68);
+            this.textBox_mubiao.Name = "textBox_mubiao";
+            this.textBox_mubiao.Size = new System.Drawing.Size(353, 21);
+            this.textBox_mubiao.TabIndex = 2;
+            // 
+            // mubiao
+            // 
+            this.mubiao.Location = new System.Drawing.Point(371, 68);
+            this.mubiao.Name = "mubiao";
+            this.mubiao.Size = new System.Drawing.Size(75, 23);
+            this.mubiao.TabIndex = 3;
+            this.mubiao.Text = "目标文件";
+            this.mubiao.UseVisualStyleBackColor = true;
+            this.mubiao.Click += new System.EventHandler(this.mubiao_Click);
+            // 
+            // begin
+            // 
+            this.begin.Location = new System.Drawing.Point(12, 132);
+            this.begin.Name = "begin";
+            this.begin.Size = new System.Drawing.Size(75, 23);
+            this.begin.TabIndex = 4;
+            this.begin.Text = "开始检测";
+            this.begin.UseVisualStyleBackColor = true;
+            this.begin.Click += new System.EventHandler(this.begin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 182);
+            this.Controls.Add(this.begin);
+            this.Controls.Add(this.mubiao);
+            this.Controls.Add(this.textBox_mubiao);
+            this.Controls.Add(this.liulan);
+            this.Controls.Add(this.textBox_mulu);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文件检测";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_mulu;
+        private System.Windows.Forms.Button liulan;
+        private System.Windows.Forms.TextBox textBox_mubiao;
+        private System.Windows.Forms.Button mubiao;
+        private System.Windows.Forms.Button begin;
+    }
+}
+
---
+++ 文件检测/文件检测/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace 文件检测
+{
+    public partial class Form1 : Form
+    {
+        static string foldPath = "";
+        static string file = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists("data.bin") == true)
+            {
+                //StreamReader reader = new StreamReader("data.bin", Encoding.Default);
+                //int i = 0;
+                ////循环读取所有行
+                //while (!reader.EndOfStream)
+                //{
+                //    if (i == 0)
+                //        textBox_mulu.Text = reader.ReadLine();
+                //    if (i == 1)
+                //        textBox_mubiao.Text = reader.ReadLine();
+                //    i++;
+                //}
+                ////关闭文件
+                //reader.Close();
+
+            }
+            else
+            {
+                FileStream myFs = new FileStream("data.bin", FileMode.Create);
+                StreamWriter mySw = new StreamWriter(myFs);
+                mySw.Close();
+                myFs.Close();
+            }
+        }
+        /// <summary>
+        /// 浏览设置检测文件夹
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void liulan_Click(object sender, EventArgs e)
+        {
+            
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                foldPath = dialog.SelectedPath;
+                MessageBox.Show("已选择文件夹:" + foldPath, "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            textBox_mulu.Text = foldPath;
+            FileStream aFile = new FileStream("data.bin", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(aFile);
+            sw.WriteLine(foldPath);
+            sw.Close();
+            aFile.Close();
+        }
+
+        private void mubiao_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择目标文件";
+            fileDialog.Filter = "所有文件(*.txt)|*.txt";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                file = fileDialog.FileName;
+                MessageBox.Show("已选择文件:" + file, "选择文件提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            textBox_mubiao.Text = file;
+            FileStream aFile = new FileStream("data.bin", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(aFile);
+            sw.WriteLine(file);
+            sw.Close();
+            aFile.Close();
+        }
+
+        private void begin_Click(object sender, EventArgs e)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = foldPath;
+            watcher.Filter = "*.txt";
+            watcher.Changed += new FileSystemEventHandler(OnProcess);
+            watcher.Created += new FileSystemEventHandler(OnProcess);
+            watcher.Deleted += new FileSystemEventHandler(OnProcess);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+            watcher.EnableRaisingEvents = true;
+            /*watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.LastAccess
+                                   | NotifyFilters.LastWrite | NotifyFilters.Security | NotifyFilters.Size;*/
+            watcher.NotifyFilter = NotifyFilters.Size;//数据写入
+            watcher.IncludeSubdirectories = true;
+        }
+
+        private static void OnProcess(object source, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Created)
+            {
+                OnCreated(source, e);
+            }
+            else if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                OnChanged(source, e);
+            }
+            else if (e.ChangeType == WatcherChangeTypes.Deleted)
+            {
+                OnDeleted(source, e);
+            }
+
+        }
+        private static void OnCreated(object source, FileSystemEventArgs e)
+        {
+            
+        }
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+           // Console.WriteLine("文件改变事件处理逻辑{0}  {1}  {2}", e.ChangeType, e.FullPath, e.Name);
+            StreamReader reader = new StreamReader(foldPath + "\\" + e.Name, Encoding.Default);
+            string newyzm = "";
+            while (!reader.EndOfStream)
+            {//读取最后一行文本
+                newyzm = reader.ReadLine();
+            }
+            //关闭文件
+            reader.Close();
+
+            //写入文件
+            FileStream aFile = new FileStream(file, FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(aFile, Encoding.GetEncoding("GB2312"));
+            sw.WriteLine(newyzm);
+            sw.Close();
+            aFile.Close();
+        }
+
+        private static void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            //Console.WriteLine("文件删除事件处理逻辑{0}  {1}   {2}", e.ChangeType, e.FullPath, e.Name);
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            //Console.WriteLine("文件重命名事件处理逻辑{0}  {1}  {2}", e.ChangeType, e.FullPath, e.Name);
+        }
+    }
+}
